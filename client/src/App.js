046940e722368587380d98248e82250f7beab62b
Abah <abahwisdom@gmail.com>
2020-10-24 12:59:22 +0100

    Added lazy loading and font preconnect

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
-import React, { useEffect } from 'react';
-import Protected from './Protected';
+import React, { useEffect, Suspense } from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-import SignIn from './components/auth/pages/sign-in';
-import SignUp from './components/auth/pages/sign-up';
-import ResetPassword from './components/auth/pages/reset-password';
-
 import store from './redux/store';
 import { loadUser } from './redux/actions/authActions.js';
+
+// import Protected from './Protected';
+// import SignIn from './components/auth/pages/sign-in';
+import SignUp from './components/auth/pages/sign-up';
+// import ResetPassword from './components/auth/pages/reset-password'
+
+const Protected= React.lazy(() => import('./Protected'));
+const SignIn= React.lazy(() => import('./components/auth/pages/sign-in'));
+// const SignUp= React.lazy(() => import('./components/auth/pages/sign-up'));
+const ResetPassword= React.lazy(() => import('./components/auth/pages/reset-password'));
+
+
 
 
 const App=(props)=>{
@@ -21,6 +28,7 @@ const App=(props)=>{
         
         <>
             <Router>
+                <Suspense fallback= {null}>
                 <Switch>
                     <Route path='/home'>
                         <Protected/>
@@ -38,6 +46,7 @@ const App=(props)=>{
                         <ResetPassword/>
                     </Route>
                 </Switch>
+                </Suspense>
         
             </Router>
         </>
